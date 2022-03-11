ed38cf41b953d5e5f672e3e2d021ea4e459cb00d
KhalidNusserat <KhMKhNu@gmail.com>
2022-03-11 17:40:14 +0300

    created TaskInfo

---
+++ src/TaskInfo.java
@@ -0,0 +1,29 @@
+public class TaskInfo {
+    private final String ID;
+    private final TaskPriority priority;
+    private final int creationTime;
+    private final int requiredTime;
+
+    public TaskInfo(String id, TaskPriority priority, int creationTime, int requiredTime) {
+        ID = id;
+        this.priority = priority;
+        this.creationTime = creationTime;
+        this.requiredTime = requiredTime;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public int getCreationTime() {
+        return creationTime;
+    }
+
+    public int getRequiredTime() {
+        return requiredTime;
+    }
+}
