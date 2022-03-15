3da291de6bd073ae61facc610d69cc092ced4ea6
KhalidNusserat <KhMKhNu@gmail.com>
2022-03-15 20:01:00 +0300

    created CsvLogWriter

---
+++ src/CsvLogWriter.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.FileWriter;
+
+public class CsvLogWriter implements LogWriter {
+    @Override
+    public void writeToFile(String path, String[][] data) {
+        try{
+            File file = new File(path);
+            file.createNewFile();
+            FileWriter fileWriter = new FileWriter(file);
+            for (String[] row : data) {
+                for (int j = 0; j < data[0].length; j++) {
+                    fileWriter.write(row[j]);
+                    if (j < data[0].length - 1)
+                        fileWriter.write(',');
+                }
+                fileWriter.write('\n');
+            }
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
