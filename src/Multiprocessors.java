cd41253a7b4a467764e9067bd5acdbfbca64f185
KhalidNusserat <KhMKhNu@gmail.com>
2022-03-15 12:44:27 +0300

    created Multiprocessors

---
+++ src/Multiprocessors.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Multiprocessors {
+
+    private final Processor[] processors;
+
+
+    public Multiprocessors(int numberOfProcessors) {
+        processors = new Processor[numberOfProcessors];
+        for (int i = 0; i < numberOfProcessors; i++)
+            processors[i] = new Processor(Integer.toString(i));
+    }
+
+    public void watchAllProcessors(GlobalLogger globalLogger) {
+        for (Processor processor : processors)
+            globalLogger.watchProcessor(processor);
+    }
+
+    public boolean isAllIdle() {
+        return Arrays.stream(processors).allMatch(Processor::isIdle);
+    }
+
+    public void executeAllProcessors() {
+        for (Processor processor : processors)
+            processor.executeOneCycle();
+    }
+}
