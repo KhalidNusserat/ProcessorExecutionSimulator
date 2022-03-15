e41458dd2755fdb22daade1d12ed5d9de6b62258
KhalidNusserat <KhMKhNu@gmail.com>
2022-03-15 22:53:13 +0300

    CsvLogWriter now deletes files if they exist, and appends .csv automatically

--- src/CsvLogWriter.java
+++ src/CsvLogWriter.java
@@ -4,19 +4,21 @@ import java.io.FileWriter;
 public class CsvLogWriter implements LogWriter {
   @Override
   public void writeToFile(String path, String[][] data) {
+    path += ".csv";
     try {
       File file = new File(path);
-      if (file.createNewFile()) {
-        FileWriter fileWriter = new FileWriter(file);
-        for (String[] row : data) {
-          for (int j = 0; j < data[0].length; j++) {
-            fileWriter.write(row[j]);
-            if (j < data[0].length - 1) fileWriter.write(',');
-          }
-          fileWriter.write('\n');
+      if (file.exists())
+        file.delete();
+      file.createNewFile();
+      FileWriter fileWriter = new FileWriter(file);
+      for (String[] row : data) {
+        for (int j = 0; j < data[0].length; j++) {
+          fileWriter.write(row[j]);
+          if (j < data[0].length - 1) fileWriter.write(',');
         }
-        fileWriter.close();
+        fileWriter.write('\n');
       }
+      fileWriter.close();
     } catch (Exception e) {
       e.printStackTrace();
     }
